acfc76c864eb1cef925cb6004c1dfc65c2081c82
Thomas Thomas <thomas.thomas@diamond.ac.uk>
2024-02-06 02:59:17 +0000

    Calibration and Adaptive Threshold

--- src/calibration.py
+++ src/calibration.py
@@ -1,24 +1,33 @@
 import cv2
-from Canny_Edge import thresholding
+from Canny_Edge import thresholding,dewarp,adding_circles
 from typing import List, Dict
 import numpy as np
+import json
+from PIL import Image
+from requests import get
+from io import BytesIO
 
-a: list[list[int]] = []
+dewarp_coord: list[list[int]] = []
 
 
 def click_event(event, x, y, flags, params):
     # checking for left mouse clicks
     if event == cv2.EVENT_LBUTTONDOWN:
         print(f"Pointer at: {x},{y}")
-        a.append([x, y])
+        dewarp_coord.append([x, y])
 
 
 def calibration():
     """Function for the user to choose 4 differnt points in the code that will allow the"""
 
-    dir_path = "data/task_lights_on"
-    image = cv2.imread(f"{dir_path}/1.jpg", 0)
-    image = cv2.resize(image, (0, 0), fx=0.6, fy=0.6)
+    url = "http://bl15j-di-serv-01.diamond.ac.uk:8087/JCAM3.mjpg.jpg"
+    response = get(url)
+    if response.status_code != 200:
+        print("Camera not available")
+    else:
+        image = Image.open(BytesIO(response.content))
+        image = cv2.cvtColor(np.asarray(image),cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image,(0, 0), fx=0.6, fy=0.6)
     cv2.namedWindow("Point Coordinates")
     y = cv2.setMouseCallback("Point Coordinates", click_event)
 
@@ -29,51 +38,17 @@ def calibration():
             break
     cv2.destroyAllWindows()
 
-    x = thresholding(image)
-    a50_edges_gauss = cv2.GaussianBlur(x["50"], (5, 5), 0)
-
-    detected_circles = cv2.HoughCircles(
-        a50_edges_gauss,
-        cv2.HOUGH_GRADIENT,
-        dp=1,
-        minDist=210,
-        param1=250,
-        param2=25,
-        minRadius=75,
-        maxRadius=130,
-    )
-
-    if detected_circles is not None:
-        # Convert the circle parameters a, b and r to integers.
-        detected_circles = np.uint16(np.around(detected_circles))
-        centers: List[List[int]] = []
-        for pt in detected_circles[0, :]:
-            x, y, r = pt[0], pt[1], pt[2]
-            centers.append([x, y, r])
-
-    # Sorting Centers of circles
-    centers = sorted(centers, key=lambda x: x[1])
-    centers = [
-        sorted(centers[0:3], key=lambda x: x[0]),
-        sorted(centers[3:6], key=lambda x: x[0]),
-        sorted(centers[6:11], key=lambda x: x[0]),
-        sorted(centers[11:16], key=lambda x: x[0]),
-        sorted(centers[16:], key=lambda x: x[0]),
-    ]
-    centers_sorted: Dict[int, List[int]] = {}
-
-    counter: int = 1
-    for i in range(len(centers)):
-        for j in centers[i]:
-            centers_sorted[counter] = j
-            counter += 1
-
-    # a = input coordinates, centers
-    # OUTPUT AS A JSON (pythoninbuilt module json)
-    # https://docs.python.org/3/library/json.html
-    # JSON dumps and JSON loads
-    # Exports as filename
-    return a, centers_sorted
+    while True:
+        plot = input("Would you like to see the dewarped image? Y/N ").upper()
+        if plot == "Y":
+            image_dewarp = dewarp(image,dewarp_coord)
+            cv2.imshow("Dewarped", image_dewarp)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            break
+    
+    with open("coordinates.json","w") as file:
+        json.dump(dewarp_coord,file)
 
 
 calibration()
--- src/main.py
+++ src/main.py
@@ -1,30 +1,50 @@
 import os
 import cv2
 from Canny_Edge import Canning, dewarp
+import json
+from PIL import Image
+from requests import get
+from io import BytesIO
+import numpy as np
+from fastapi import FastAPI
+
+app = FastAPI()
 
 def main():
-    # folder path: Code that is used to collect images. This will change to the camera stream
-    dir_path = "data/task_lights_on"
-    count = 0
-    # Iterate directory
-    for path in os.listdir(dir_path):
-        # check if current path is a file
-        if os.path.isfile(os.path.join(dir_path, path)):
-            count += 1
+    # Call image from live camera feed
+    url = "http://bl15j-di-serv-01.diamond.ac.uk:8087/JCAM3.mjpg.jpg"
+    response = get(url)
+    if response.status_code != 200:
+        print("Camera not available")
+    else:
+        image = Image.open(BytesIO(response.content))
+        image = cv2.cvtColor(np.asarray(image),cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image,(0, 0), fx=0.6, fy=0.6)
 
-    input_coordinates = [[120, 24], [150, 1202], [1332, 1211], [1330, 0]]
-    # call this from JSON.
+    with open("coordinates.json","r") as file:
+        input_coordinates = json.load(file)
 
-    for i in range(count):
-        # this part of the code will be replaced with collecting images from the stream.
-        # image = cv2.imread(f"{dir_path}/{i+1}.jpg", 0)
-        image = cv2.imread("nothing.jpg", 0)
-        image = cv2.resize(image, (0, 0), fx=0.6, fy=0.6)
-        dewarped = dewarp(image, input_coordinates)
-        output = Canning(dewarped)
-        # cv2.imshow("Output", output)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+
+    # # Call from images
+    # dir_path = "data/task_lights_on"
+    # count = 0
+    # # Iterate directory
+    # for path in os.listdir(dir_path):
+    #     # check if current path is a file
+    #     if os.path.isfile(os.path.join(dir_path, path)):
+    #         count += 1
+
+    # for i in range(count):
+    #     # this part of the code will be replaced with collecting images from the stream.
+    #     image = cv2.imread(f"{dir_path}/{i+1}.jpg", 0)
+    #     image = cv2.resize(image, (0, 0), fx=0.6, fy=0.6)
+
+        
+    dewarped = dewarp(image, input_coordinates)
+    output = Canning(dewarped)
+    # cv2.imshow("Output", output)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
